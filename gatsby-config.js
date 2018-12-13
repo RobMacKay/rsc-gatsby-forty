6738e026896fb99c9ac59ea7e447cf0c6d5e3327
Rob MacKay <robskiwarrior@gmail.com>
2018-12-13 18:44:51 +0000

    Woop woop

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,18 +6,18 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: 'gatsby-starter-default',
-    //     short_name: 'starter',
-    //     start_url: '/',
-    //     background_color: '#663399',
-    //     theme_color: '#663399',
-    //     display: 'minimal-ui',
-    //     icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'RSC Media',
+        short_name: 'RSCMedia',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/website-icon.png', //This path is relative to the root of the site.
+      },
+    },
     'gatsby-plugin-sass',
     //'gatsby-plugin-offline'
   ],
