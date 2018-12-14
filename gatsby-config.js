69d7596d8a6dc87db51d170977991a31dc3a3dc9
Rob MacKay <robskiwarrior@gmail.com>
2018-12-14 09:11:10 +0000

    adding twitter

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
         name: 'RSC Media',
         short_name: 'RSCMedia',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#242943',
+        theme_color: '#242943',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', //This path is relative to the root of the site.
       },
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="icons">
-                <li><a href="https://twitter.com/svgrob" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="https://www.instagram.com/robatcopter/" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                <li><a href="https://twitter.com/rscmedialtd" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+                {/* <li><a href="https://www.instagram.com/robatcopter/" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li> */}
             </ul>
             <ul className="copyright">
                 <li>&copy; RSC Media Ltd - 4 North Guildry Street, Elgin, United Kingdom, IV30 1JR - No: SC614929</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
