7cf9f7046d98ef75ceb6d9263d6e19c18208c42c
Rob MacKay <robskiwarrior@gmail.com>
2019-12-02 22:10:40 +0000

    Lets go

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
+import logo from '../assets/images/logo.png'
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>RSC Media</strong> <span>Development, Applications &amp; Consultation</span></Link>
-        
+        <Link to="/" className="logo"><img src={logo} alt="RSC Media Logo" /> <span>Development, Applications &amp; Consultation</span></Link>
     </header>
 )
 
